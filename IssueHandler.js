bfc54c07356c04cf5bf24787d17e3d70071ab3f6
Shikaga <shikaga@gmail.com>
2015-04-03 20:46:04 +0100

    Putting accidentally removed 'legacy' code back in

---
+++ IssueHandler.js
@@ -0,0 +1,52 @@
+define(['StatisticsCalculator'], function(StatisticsCalculator) {
+  function IssueHandler() {
+    this.issues = [];
+  }
+
+  IssueHandler.prototype.addIssue = function(issue) {
+    this.issues.push(issue);
+  }
+
+  IssueHandler.prototype.getIssue = function(issueId) {
+    for (var i=0; i < this.issues.length; i++) {
+      var issue = this.issues[i];
+      if (issue.id.match(/\d+/)[0] === issueId) {
+        return issue;
+      }
+    }
+    return null;
+  }
+
+  getDataWithJSON = function(callback, username, password, requestUrl) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://cors-anywhere.herokuapp.com/" + requestUrl)
+    if (this.username !== "") {
+      this.setAuthorizationHeader(xhr, username, password);
+    }
+    xhr.setRequestHeader("x-requested-with", "love");
+    xhr.send();
+    xhr.onload = function(response) {
+      if (this.status === 401) {
+        callback(this.status);
+      } else {
+        //var data = JSON.parse(response.target.response);
+
+        //callback(data);
+        window.localStorage.setItem("jiraData", response.target.response);
+        statisticsCalculator = new StatisticsCalculator(JSON.parse(window.localStorage.getItem("jiraData")).issues);
+        alert("Complete! Check out the 'statisticsCalculator' object! It will now be available on refresh.")
+      }
+    }
+  };
+
+  setAuthorizationHeader = function(xhr, username, password) {
+    var authHeader = "Basic "+btoa(username + ":" + password);
+    xhr.setRequestHeader("Authorization", authHeader);
+  }
+
+  if (window.localStorage.getItem("jiraData")) {
+    statisticsCalculator = new StatisticsCalculator(JSON.parse(window.localStorage.getItem("jiraData")).issues);
+  }
+
+  return IssueHandler;
+});
--- JiraStatisticsCompilerPanel.js
+++ JiraStatisticsCompilerPanel.js
@@ -2,8 +2,59 @@ define(['lib/react', 'LoginPanel'], function(React, LoginPanel) {
   var JiraStatisticsCompilerPanel = React.createClass({
     render: function() {
       return React.DOM.div({className: "container"}, LoginPanel({login: this.login}));
-    }
+    },
+    getInitialState: function() {
+      return {
+        issueId: "",
+        selectedIssueId: null,
+        issuesChanged: [],
+        mode: "login",
+        projectData: null,
+        issues: null
+      }
+    },
+    selectProject: function(projectId) {
+      getDataWithJSON(function(projectData) {
+        console.log("data", projectData)
+        var getOrderedIssues = function(x) {
+          var issues = [];
+          x.issues.forEach(function(issue) {
+            var finalState = null;
+            var finalDate = null;
+            issue.changelog.histories.forEach(function(history) {
+            var dateOccured = this.parseDate(history.created);
+            history.items.forEach(function(item) {
+              if (item.field == "status" && item.toString == "Resolved") {
+                finalState = item.toString;
+                finalDate = dateOccured;
+              }
+            }.bind(this));
+          }.bind(this))
+          issues.push({summary: issue.fields.summary, key: issue.key, createdDate: this.parseDate(issue.fields.created), resolvedDate: finalDate})
+        }.bind(this))
+          this.setState({issues: issues});
+        }.bind(this)
+
+        getOrderedIssues(projectData);
+
+      }.bind(this), this.state.username, this.state.password, this.state.url + "/rest/api/latest/search?jql=project=" + window.location.search.substr(1) + " AND issuetype=Bug&maxResults=1000&expand=changelog");
+    },
+    login: function(url, username, password) {
+        window.url = url;
+        this.state.url = url;
+        this.state.username = username;
+        this.state.password = password;
+        this.setState({
+          url: url,
+          username: username,
+          password: password,
+          mode: "loggedIn"
+        });
+        this.selectProject(80);
+      }
   });
+
+
 
   return JiraStatisticsCompilerPanel;
 });
