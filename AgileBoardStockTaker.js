dc034e4d277d607cf784835d6150080c92cdd193
Shikaga <shikaga@gmail.com>
2015-04-03 20:14:43 +0100

    Moved React code to own file

---
+++ AgileBoardStockTaker.js
@@ -0,0 +1,150 @@
+var LoginPanel = React.createClass({
+  getInitialState: function() {
+    return {
+      url: "https://jira.caplin.com",
+      username: "jonp",
+      password: ""
+    }
+  },
+  changeUrl: function(e) {
+    this.setState({
+      url: e.target.value
+    });
+  },
+  changeUsername: function(e) {
+    this.setState({
+      username: e.target.value
+    });
+  },
+  changePassword: function(e) {
+    this.setState({
+      password: e.target.value
+    });
+  },
+  handleKeyPress: function(e,f,g) {
+    if (e.which == 13) {
+      this.login();
+    }
+  },
+  login: function() {
+    this.props.login(this.state.url, this.state.username, this.state.password);
+  },
+  render: function() {
+    return React.DOM.div({className: "panel panel-default"},
+      React.DOM.div({className: "panel-body"},
+        React.DOM.div({className: "input-group"},
+          React.DOM.span({className: "input-group-addon"}, "Jira URL"),
+          React.DOM.input({type: "text", onChange: this.changeUrl, value: this.state.url, className: "form-control"})
+        ),
+        React.DOM.br(),
+        React.DOM.div({className: "input-group"},
+          React.DOM.span({className: "input-group-addon"}, "Username"),
+          React.DOM.input({type: "text", onChange: this.changeUsername, value: this.state.username, className: "form-control"})
+        ),
+        React.DOM.br(),
+        React.DOM.div({className: "input-group"},
+          React.DOM.span({className: "input-group-addon"}, "Password"),
+          React.DOM.input({type: "password", onKeyUp: this.handleKeyPress, onChange: this.changePassword, value: this.state.password, className: "form-control"})
+        ),
+        React.DOM.div({className: "panel-body"},
+          React.DOM.div({className: "col-xs-12 col-md-12"},
+            React.DOM.button({type: "button", onClick: this.login, className: "btn btn-success btn-block"}, "Login")
+          )
+        )
+      )
+    );
+  }
+});
+
+
+var AgileBoardStockTaker = React.createClass({
+  getInitialState: function() {
+    return {
+      issueId: "",
+      selectedIssueId: null,
+      issuesChanged: [],
+      mode: "login",
+      projectData: null,
+      issues: null
+    }
+  },
+  doneClicked: function() {
+    this.setState({
+      selectedIssueId: this.state.issueId,
+      issueId: ""
+    });
+  },
+  completeClicked: function() {
+    this.setState({
+      mode: "changed"
+    });
+  },
+  numberClicked: function(number) {
+    this.setState({
+      issueId: this.state.issueId + number
+    })
+  },
+  issueChanged: function(data) {
+    this.setState({
+      issuesChanged: this.state.issuesChanged.concat(data)
+    });
+  },
+  moreClicked: function() {
+    this.setState({
+      mode: "input"
+    });
+  },
+  login: function(url, username, password) {
+    window.url = url;
+    this.state.url = url;
+    this.state.username = username;
+    this.state.password = password;
+    this.setState({
+      url: url,
+      username: username,
+      password: password,
+      mode: "loggedIn"
+    });
+    this.selectProject(80);
+    // getDataWithJSON(function(data) {
+    // 	var projects = [];
+    // 	data.views.forEach(function(view) {
+    // 		console.log(view.name, view.id, view.key, view);
+    // 		projects.push({name: view.name, id: view.id, key: view.key})
+    // 	});
+    // 	this.setState({projectData: projects});
+    // }.bind(this), username, password, url + "/rest/greenhopper/1.0/rapidviews/viewsData.json");
+  },
+  saveLoginDetails: function() {
+
+  },
+  selectProject: function(projectId) {
+    getDataWithJSON(function(projectData) {
+      console.log("data", projectData)
+      var getOrderedIssues = function(x) {
+        var issues = [];
+        x.issues.forEach(function(issue) {
+          var finalState = null;
+          var finalDate = null;
+          issue.changelog.histories.forEach(function(history) {
+          var dateOccured = this.parseDate(history.created);
+          history.items.forEach(function(item) {
+            if (item.field == "status" && item.toString == "Resolved") {
+              finalState = item.toString;
+              finalDate = dateOccured;
+            }
+          }.bind(this));
+        }.bind(this))
+        issues.push({summary: issue.fields.summary, key: issue.key, createdDate: this.parseDate(issue.fields.created), resolvedDate: finalDate})
+      }.bind(this))
+        this.setState({issues: issues});
+      }.bind(this)
+
+      getOrderedIssues(projectData);
+
+    }.bind(this), this.state.username, this.state.password, this.state.url + "/rest/api/latest/search?jql=project=" + window.location.search.substr(1) + " AND issuetype=Bug&maxResults=1000&expand=changelog");
+  },
+  render: function() {
+    return React.DOM.div({className: "container"}, LoginPanel());
+  }
+});
